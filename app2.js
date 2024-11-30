2e34ba144d6772e53253df854a755634c1c2062a
Albane0 <gardeleinalbane@gmail.com>
2024-11-30 22:59:25 +0100

    test

---
+++ app2.js
@@ -0,0 +1,48 @@
+let nextBtn2 = document.querySelector('.slider2 .next2')
+let prevBtn2 = document.querySelector('.slider2 .prev2')
+
+let slider2 = document.querySelector('.slider2')
+let sliderList2 = slider2.querySelector('.slider2 .list2')
+let thumbnail2 = document.querySelector('.slider2 .thumbnail2')
+let thumbnailItems2 = thumbnail2.querySelectorAll('.item')
+
+thumbnail2.appendChild(thumbnailItems2[0])
+
+// Function for next button 
+nextBtn2.onclick = function() {
+    moveSlider2('next2')
+}
+
+
+// Function for prev button 
+prevBtn2.onclick = function() {
+    moveSlider2('prev2')
+}
+
+
+function moveSlider2(direction) {
+    let sliderItems2 = sliderList2.querySelectorAll('.item')
+    let thumbnailItems2 = document.querySelectorAll('.thumbnail2 .item')
+    
+    if(direction === 'next2'){
+        sliderList2.appendChild(sliderItems2[0])
+        thumbnail2.appendChild(thumbnailItems2[0])
+        slider2.classList.add('next2')
+    } else {
+        sliderList2.prepend(sliderItems2[sliderItems2.length - 1])
+        thumbnail2.prepend(thumbnailItems2[thumbnailItems2.length - 1])
+        slider2.classList.add('prev2')
+    }
+
+
+    slider2.addEventListener('animationend', function() {
+        if(direction === 'next2'){
+            slider2.classList.remove('next2')
+        } else {
+            slider2.classList.remove('prev2')
+        }
+    }, {once: true}) // Remove the event listener after it's triggered once
+}
+
+
+
---
+++ countdown.js
@@ -0,0 +1,39 @@
+let datecountdown = new Date("March 22, 2025 08:00:00").getTime()
+            
+            let x = setInterval(function(){
+
+                let mtn = new Date().getTime();
+                let tpsrestant = datecountdown - mtn;
+                let jours = Math.floor(tpsrestant / (1000*60*60*24));
+                let heures = Math.floor(tpsrestant % (1000*60*60*24) / (1000*60*60));
+                let minutes = Math.floor(tpsrestant % (1000*60*60) / (1000*60));
+                let secondes = Math.floor(tpsrestant % (1000*60) / (1000));
+
+                document.querySelector(".countdown").innerHTML =`
+                    <div class="boite">
+                        <span class="valeur">${jours}</span>
+                        <br>
+                        <span class="unite">jours</span>
+                    </div>
+                    <div class="boite">
+                        <span class="valeur">${heures}</span>
+                        <br>
+                        <span class="unite">heures</span>
+                    </div>                    
+                    <div class="boite">
+                        <span class="valeur">${minutes}</span>
+                        <br>
+                        <span class="unite">minutes</span>
+                    </div>                    
+                    <div class="boite">
+                        <span class="valeur">${secondes}</span>
+                        <br>
+                        <span class="unite">secondes</span>
+                    </div>
+                `;
+                if (tpsrestant < 1){
+                    clearInterval(x);
+                    jours, heures, minutes, secondes= 0
+                }
+            }, 1000);
+
